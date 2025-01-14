dc7aae125f3e8aa2a87762f29c0ba49db1090a56
Philippe Blouin <philblouin@me.com>
2025-01-14 10:32:55 -0500

    Create tdb.py
    tdb pour récupération des données

---
+++ src/tdb.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import json
+import os
+
+
+def ouvrirApplications():
+  pass
+
+def ouvrirServeur():
+  pass
+
+def ouvrirPerformance():
+  pass
+
+
+def main():
+  pass
+
+if __name__ == '__main__':
+  main()
