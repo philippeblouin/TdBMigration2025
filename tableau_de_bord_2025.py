31002eba14232bb990584abe9f8029f60f381458
Philippe Blouin <philblouin@me.com>
2025-01-14 10:54:50 -0500

    Update tableau_de_bord_2025.py
    refactoring

--- tableau_de_bord_2025.py
+++ tableau_de_bord_2025.py
@@ -1,30 +1,45 @@
 import streamlit as st
+import streamlit_extras.metric_cards import style_metric_cards
+
 import json
 from src import *
 
+def main():
+    # Titre de l'application
+    st.header("002 - TdB de migration 2025")
 
-# Titre de l'application
-st.title("001 - Suivi de la migration des charges vers le Cloud")
+    if "role" not in st.session_state:
+        st.session_state.role = None
+    
+    ROLES = [None,"Demandeur","Réception", "Admin"]
 
-# Description
-st.markdown("""
-Ce tableau de bord permet de suivre l'avancement de la migration des charges vers le cloud. Vous pouvez consulter
-les progrès, les performances, et les problèmes rencontrés pendant la migration.
-""")
+def inscriptions():
+    # Description
+    st.markdown("""
+    Ce tableau de bord permet de suivre l'avancement de la migration des charges vers le cloud. Vous pouvez consulter
+    les progrès, les performances, et les problèmes rencontrés pendant la migration.
+    """)
 
-# Section des alertes ou des problèmes rencontrés
-st.subheader("Alertes et problèmes")
-problèmes = ["Problème de performance sur le Serveur C", "Erreur de configuration sur le Serveur B"]
-for problème in problèmes:
-    st.markdown(f"- {problème}")
+    # Section des alertes ou des problèmes rencontrés
+    st.subheader("Alertes et problèmes")
+    problèmes = ["Problème de performance sur le Serveur C", "Erreur de configuration sur le Serveur B"]
+    for problème in problèmes:
+        st.markdown(f"- {problème}")
 
-# Affichage d'une carte de l'utilisation des ressources
-st.subheader("Utilisation des ressources cloud")
-utilisation_resources = {
-    'Ressource': ['CPU', 'Mémoire', 'Stockage'],
-    'Utilisation (%)': [65, 70, 85]
-}
+    # Affichage d'une carte de l'utilisation des ressources
+    st.subheader("Utilisation des ressources cloud")
+    utilisation_resources = {
+        'Ressource': ['CPU', 'Mémoire', 'Stockage'],
+        'Utilisation (%)': [65, 70, 85]
+                            }
 
-# Bouton pour simuler la mise à jour des données
-if st.button("Mettre à jour les données de migration"):
-    st.write("Données mises à jour avec succès!")
+    # Bouton pour simuler la mise à jour des données
+    if st.button("Mettre à jour les données de migration"):
+        st.write("Données mises à jour avec succès!")
+
+
+if __name__ == "__main__":
+    main()
+    inscription()
+
+    
